cf10064ee82baa9f275fe2308bcb2515b68519e6
yvonneoyella <yvonneoyellaa@gmail.com>
2024-02-08 07:06:59 +0300

    added students details

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+
+
+fun main() {
+    var name = "Acen Juliet"
+    println(name)
+    var age: Byte = 25
+    println(age)
+    var phoneNumber = 256786001664
+    println(phoneNumber)
+    var weightKg=75
+    println(weightKg)
+    val citizenOfKenya= false
+    println(citizenOfKenya)}
